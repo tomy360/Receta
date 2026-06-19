c4ffdd4ecb02c81099d737557928db7be50c7341
tomy360 <tomilamaja@gmail.com>
2026-06-19 15:14:53 -0300

    Add files via upload

--- JS/auth.js
+++ JS/auth.js
@@ -78,23 +78,22 @@ function avatarHtml(size) {
   var url = sesion.avatarUrl;
   var inicial = sesion.username.charAt(0).toUpperCase();
   var fontSize = Math.round(size * 0.45);
-  var estiloBase = 'width:' + size + 'px;height:' + size + 'px;border-radius:50%;';
+  var estiloBase = 'width:' + size + 'px;height:' + size + 'px;border-radius:50%;display:inline-flex;align-items:center;justify-content:center;color:#fff;font-weight:600;font-size:' + fontSize + 'px;flex-shrink:0;background-size:cover;background-position:center;';
   if (url) {
-    return '<span style="display:inline-block;position:relative;' + estiloBase + '">' +
-      '<span class="avatar-inicial" style="position:absolute;inset:0;display:inline-flex;align-items:center;justify-content:center;background:var(--verde);color:#fff;font-weight:600;font-size:' + fontSize + 'px;">' + inicial + '</span>' +
-      '<img src="' + url + '" class="avatar-img" style="position:absolute;inset:0;width:100%;height:100%;object-fit:cover;" alt="Avatar" onerror="this.style.display=\'none\'">' +
-      '</span>';
+    return '<span class="avatar-inicial" style="' + estiloBase + 'background-color:var(--verde);background-image:url(\'' + url.replace(/'/g, "\\'") + '\');">' + inicial + '</span>';
   }
-  return '<span class="avatar-inicial" style="' + estiloBase + 'display:inline-flex;align-items:center;justify-content:center;background:var(--verde);color:#fff;font-weight:600;font-size:' + fontSize + 'px;flex-shrink:0;">' + inicial + '</span>';
+  return '<span class="avatar-inicial" style="' + estiloBase + 'background:var(--verde);">' + inicial + '</span>';
 }
 
 function avatarHtmlFor(username, avatarUrl, size) {
   size = size || 32;
   var inicial = (username || '?').charAt(0).toUpperCase();
+  var fontSize = Math.round(size * 0.45);
+  var estiloBase = 'width:' + size + 'px;height:' + size + 'px;border-radius:50%;display:inline-flex;align-items:center;justify-content:center;color:#fff;font-weight:600;font-size:' + fontSize + 'px;flex-shrink:0;background-size:cover;background-position:center;';
   if (avatarUrl) {
-    return '<img src="' + avatarUrl + '" class="avatar-img" style="width:' + size + 'px;height:' + size + 'px;border-radius:50%;object-fit:cover;" alt="Avatar" onerror="this.remove()">';
+    return '<span class="avatar-inicial" style="' + estiloBase + 'background-color:var(--verde);background-image:url(\'' + avatarUrl.replace(/'/g, "\\'") + '\');">' + inicial + '</span>';
   }
-  return '<span class="avatar-inicial" style="width:' + size + 'px;height:' + size + 'px;border-radius:50%;display:inline-flex;align-items:center;justify-content:center;background:var(--verde);color:#fff;font-weight:600;font-size:' + (size * 0.45) + 'px;flex-shrink:0;">' + inicial + '</span>';
+  return '<span class="avatar-inicial" style="' + estiloBase + 'background:var(--verde);">' + inicial + '</span>';
 }
 
 function actualizarUI() {
